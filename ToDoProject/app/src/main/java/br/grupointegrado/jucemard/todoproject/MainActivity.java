6147502c4db50ba741ab177cbb0075af0044e3f1
JucemarDias\Jucemar Dias <jucemar_dias@hotmail.com>
2017-04-19 19:44:59 -0300

    commit 19/04

--- ToDoProject/app/src/main/java/br/grupointegrado/jucemard/todoproject/MainActivity.java
+++ ToDoProject/app/src/main/java/br/grupointegrado/jucemard/todoproject/MainActivity.java
@@ -2,25 +2,20 @@ package br.grupointegrado.jucemard.todoproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import br.grupointegrado.jucemard.todoproject.bd.DataBase;
 import br.grupointegrado.jucemard.todoproject.model.ToDo;
 
 public class MainActivity extends AppCompatActivity {
-
     private ListView lvToDo;
     private DataBase db;
 
@@ -56,5 +51,6 @@ public class MainActivity extends AppCompatActivity {
         startActivity(i);
 
     }
+
 
 }
--- ToDoProject/app/src/main/java/br/grupointegrado/jucemard/todoproject/RegisterToDoActivity.java
+++ ToDoProject/app/src/main/java/br/grupointegrado/jucemard/todoproject/RegisterToDoActivity.java
@@ -1,20 +1,19 @@
 package br.grupointegrado.jucemard.todoproject;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
